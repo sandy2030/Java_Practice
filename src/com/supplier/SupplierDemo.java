9c9959a281c030a58d2a0272f3731fe925c30346
sandy2030 <sandeeppandey2024@gmail.com>
2022-11-17 02:16:24 +0530

    added supplier demo

---
+++ src/com/supplier/SupplierDemo.java
@@ -0,0 +1,17 @@
+package com.supplier;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierDemo {
+    // T get()  is declare in Supplier interface
+
+    // create Lambda function for supplier functional interface
+
+    public static void main(String[] args) {
+        Supplier message=()->{return "No Worries !!!";};
+        List<String> list = Arrays.asList();
+        System.out.println(list.stream().findAny().orElseGet(message));
+    }
+}
